d13afd6285f1b2415aa716d6c1ba63553521dd5c
Math Application <xoawtaka@gmail.com>
2025-10-07 14:07:26 -0400

    first commit

---
+++ src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,47 @@
+package com.pluralsight;
+
+public class Employee {
+
+    private int employeeId = 0;
+    private String name = "";
+    private double hoursWorked = 0;
+    private double payRate = 0;
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+}
+
+
---
+++ src/main/java/com/pluralsight/PayrollCalculatorApp.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class PayrollCalculatorApp {
+    public static void main(String[] args) {
+
+    }
+}
